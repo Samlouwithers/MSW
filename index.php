cef03acc4a1f43cbdfbe0a66e77cf50bd1eb9fea
Samlouwithers <samlouwithers@gmail.com>
2017-02-06 19:34:21 -0500

    added tree code test for diagram

--- index.php
+++ index.php
@@ -190,6 +190,49 @@
 		</div>
 
 	</section>
+
+
+
+
+
+	<section id="new" class="content">
+
+		<div class="tree">
+			<ul>
+				<li>
+					<a href="#">Parent</a>
+					<ul>
+						<li>
+							<a href="#">Child</a>
+							<ul>
+								<li>
+									<a href="#">Grand Child</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">Child</a>
+							<ul>
+								<li><a href="#">Grand Child</a></li>
+								<li>
+									<a href="#">Grand Child</a>
+									<ul>
+										<li><a href="#">Great Grand Child</a></li>
+										<li><a href="#">Great Grand Child</a></li>
+										<li><a href="#">Great Grand Child</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Grand Child</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+
+
 	
 
 	<footer>
